682d4dea30afe85ac77d310d92f91b28286a91c5
phela_bzbz <149921073+KOLENKIPCHELYI@users.noreply.github.com>
2025-05-31 18:51:17 +0300

    Update tg-theme.js

--- js/tg-theme.js
+++ js/tg-theme.js
@@ -1,11 +1,10 @@
-// Обработка тем Telegram
 document.addEventListener('DOMContentLoaded', () => {
     const tg = window.Telegram?.WebApp;
     if (!tg) return;
 
-    tg.expand(); // Растянуть на весь экран
+    tg.expand();
     
-    // Применяем тему Telegram
+    // Применение темы Telegram
     document.documentElement.style.setProperty('--bg-primary', tg.themeParams.bg_color || '#ffffff');
     document.documentElement.style.setProperty('--text-primary', tg.themeParams.text_color || '#212121');
     document.documentElement.style.setProperty('--bg-secondary', tg.themeParams.secondary_bg_color || '#f5f5f5');
